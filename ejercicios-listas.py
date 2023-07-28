f92946d9e868a4ed23d386c13db0862fa540b7d5
Yoselin Martinez <kymargo02@gmail.com>
2023-07-27 18:41:39 -0700

    guarde ejercicios de listas

---
+++ ejercicios-listas.py
@@ -0,0 +1,17 @@
+lista = ['voldi','norris','nena','manchis','thor','tigri','frijol','tori']
+#imprimir el ultimo nombre de la lista 
+#print(lista[-1])
+
+#imprimir machis, thor y tigri
+#print(lista[3:6])
+
+#imprimir manchis indices negativos 
+#print(lista[-5])
+
+#crear dos listas nuevas y unirlas en una lista nueva 
+lista1 = ['Kill Bill','Pulp Fiction']
+lista2 = ['Noa Centineo','Barbara Palvin']
+print(lista1)
+lista1.extend(lista2)
+print(lista1)
+
