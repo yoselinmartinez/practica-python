a88e1dbbec4d12749ca5cf8fcd3152cd470be0ab
Yoselin Martinez <kymargo02@gmail.com>
2023-07-29 12:07:35 -0700

    realice ejercicios condicionales de liverpool

---
+++ condicionales.py
@@ -0,0 +1,20 @@
+#dependiendo del anio de nacimiento imprimir mayor de edad si es mayor a 18 anios 
+anio_nacimiento = 1995
+edad = (2023 - anio_nacimiento)
+print(edad)
+
+if edad <=17:
+    print('Esta persona es menor de edad')
+ 
+else:
+    print("Esta persona es mayor de edad")
+
+#castear una cadena a entero 
+
+numero = '25'
+
+numero2 = int(numero)
+
+print(type(numero2))
+
+
---
+++ ejercicio-liverpool.py
@@ -0,0 +1,14 @@
+#edad, estudiante o no, dependiendo de la lista que pueda o no trabajar en liverpool, mayor de edad y estudiante
+
+inf_personal = [25, 'estudiante']
+
+edad = inf_personal[0]
+estudiante_o_no = inf_personal[1]
+
+if edad >=19 and estudiante_o_no == 'estudiante':
+    print('Si puede trabajar para liverpool')
+
+else:
+    print('no puede trabajar para liverpool')
+
+    
