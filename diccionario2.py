64f919cff3f73bd37537bdaa25081f8eeca3240a
Yoselin Martinez <kymargo02@gmail.com>
2023-08-02 16:03:33 -0700

    Realice ejercicios de for con diccionarios y listas

---
+++ diccionario2.py
@@ -0,0 +1,11 @@
+#crea un diccionario que ttenga 3 llves y 3 vaores
+
+paises_capitales = {
+    'canada':'otawa',
+    'estados_unidos':'washington',
+    'mexico':'cdmx'
+    }
+
+for pais in paises_capitales.keys():
+    capital = paises_capitales[pais]
+    print(capital)
---
+++ ejericio-for.py
@@ -0,0 +1,16 @@
+informacion = {
+    'nombre':'Yoselin',
+    'apellido':'Martinez',
+    'nacionalidad':'canadiense',
+    'signo':'tauro',
+    'edad':'25'
+    }
+
+#utilizando un for, imprimir todos los valores del diccionario 
+
+for llave in informacion.keys():
+    valores = informacion[llave]
+    print(valores)
+
+
+
